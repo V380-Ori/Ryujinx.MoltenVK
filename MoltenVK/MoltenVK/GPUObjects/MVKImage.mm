1075750bb81851311379f3895ae091227f267a27
squidbus <175574877+squidbus@users.noreply.github.com>
2025-10-05 20:47:45 -0700

    Remove MVK_SUPPORT_IOSURFACE_BOOL

--- MoltenVK/MoltenVK/GPUObjects/MVKImage.mm
+++ MoltenVK/MoltenVK/GPUObjects/MVKImage.mm
@@ -1067,10 +1067,8 @@ VkResult MVKImage::setMTLTexture(uint8_t planeIndex, id<MTLTexture> mtlTexture) 
 	_usage = getPixelFormats()->getVkImageUsageFlags(mtlTexture.usage, mtlTexture.pixelFormat);
 	_stencilUsage = _usage;
 
-	if (getMetalFeatures().ioSurfaces) {
-		_ioSurface = mtlTexture.iosurface;
-		if (_ioSurface) { CFRetain(_ioSurface); }
-	}
+	_ioSurface = mtlTexture.iosurface;
+	if (_ioSurface) { CFRetain(_ioSurface); }
 
 	return VK_SUCCESS;
 }
@@ -1089,10 +1087,6 @@ VkResult MVKImage::useIOSurface(IOSurfaceRef ioSurface) {
 
 	// Don't recreate existing. But special case of incoming nil if already nil means create a new IOSurface.
 	if (ioSurface && _ioSurface == ioSurface) { return VK_SUCCESS; }
-
-    if (!getMetalFeatures().ioSurfaces) { return reportError(VK_ERROR_FEATURE_NOT_PRESENT, "vkUseIOSurfaceMVK() : IOSurfaces are not supported on this platform."); }
-
-#if MVK_SUPPORT_IOSURFACE_BOOL
 
     for (uint8_t planeIndex = 0; planeIndex < _planes.size(); planeIndex++) {
         _planes[planeIndex]->releaseMTLTexture();
@@ -1151,8 +1145,6 @@ VkResult MVKImage::useIOSurface(IOSurfaceRef ioSurface) {
             CFRelease(properties);
         }
     }
-
-#endif
 
     return VK_SUCCESS;
 }
