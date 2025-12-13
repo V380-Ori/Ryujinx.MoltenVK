1cf361ffd189addec5b0dc4f7fb82a5f31b4d9a8
V380-Ori <infiniteloop0finsanity@gmail.com>
2025-12-14 01:13:08 +0200

    Disable triangle fans.
    Fixes performance regression in Resident Evil 4.

--- MoltenVK/MoltenVK/GPUObjects/MVKDevice.mm
+++ MoltenVK/MoltenVK/GPUObjects/MVKDevice.mm
@@ -595,7 +595,7 @@ void MVKPhysicalDevice::getFeatures(VkPhysicalDeviceFeatures2* features) {
 				portabilityFeatures->shaderSampleRateInterpolationFunctions = _metalFeatures.pullModelInterpolation;
 				portabilityFeatures->tessellationIsolines = false;
 				portabilityFeatures->tessellationPointMode = false;
-				portabilityFeatures->triangleFans = true;
+				portabilityFeatures->triangleFans = false;
 				portabilityFeatures->vertexAttributeAccessBeyondStride = true;	// Costs additional buffers. Should make configuration switch.
 				break;
 			}
